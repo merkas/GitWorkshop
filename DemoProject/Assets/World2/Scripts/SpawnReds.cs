91c3cb1c502a491af5da4992d789a158b10c36ab
YuneTheNoob <74174513+YuneTheNoob@users.noreply.github.com>
2020-11-09 17:12:47 +0100

    added moving camera to world1

---
+++ DemoProject/Assets/World2/Scripts/SpawnReds.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnReds : MonoBehaviour
+{
+    public GameObject red;
+
+    void Update()
+    {
+        Vector3 randomPos = new Vector3(Random.Range(-100f, 100f), 2.5f, Random.Range(-100f, 100f));
+        Instantiate(red, randomPos, Quaternion.identity);
+    }
+}
