faf327ff7a28046770884fe0b987f90a381ba869
VirtualTacco <66110667+VirtualTacco@users.noreply.github.com>
2020-11-09 13:33:41 +0100

    SpawnerRed in Timeline

--- DemoProject/Assets/World2/Scripts/SpawnReds.cs
+++ DemoProject/Assets/World2/Scripts/SpawnReds.cs
@@ -6,9 +6,9 @@ public class SpawnReds : MonoBehaviour
 {
     public GameObject red;
 
-    public void SpawnRed()
+    void Update()
     {
-        Vector3 randomPos = new Vector3(Random.Range(100f, 100f), 2.5f, Random.Range(100f, 100f));
+        Vector3 randomPos = new Vector3(Random.Range(-100f, 100f), 2.5f, Random.Range(-100f, 100f));
         Instantiate(red, randomPos, Quaternion.identity);
     }
 }
