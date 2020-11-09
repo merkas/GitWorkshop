12033eacaae853bde6203b7185c3cd847cbc98da
VirtualTacco <66110667+VirtualTacco@users.noreply.github.com>
2020-11-09 15:43:46 +0100

    Black TimedDestroy and ParticleEffectSpawner added

---
+++ DemoProject/Assets/World2/Scripts/ParticleEffectSpawner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEffectSpawner : MonoBehaviour
+{
+    public GameObject particleEffect;
+
+    public void SpawnParticleEffectOneTime()
+    {
+        Instantiate(particleEffect, gameObject.transform);
+        this.enabled = false;
+        //Destroy(this);
+    }
+}
