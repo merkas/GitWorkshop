084e75805cfb1eaf84528f1fb9bd160c01edb38a
VirtualTacco <66110667+VirtualTacco@users.noreply.github.com>
2020-11-10 11:19:39 +0100

    MercuryLib Editor and Behaviour class

---
+++ DemoProject/Assets/Scripts/Editor/MercuryLibEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MercuryLibBehaviour))]
+public class MercuryLibEditor : Editor
+{
+    //Texture texture;
+    //Texture myTexture;
+
+    public override void OnInspectorGUI()
+    {
+        var myTexture = Resources.Load("Test", typeof(Texture)) as Texture;
+
+        GUILayout.Label(myTexture);
+        //GUI.DrawTexture(new Rect(10, 10, 60, 60), myTexture);
+        DrawDefaultInspector();
+    }
+}
