1225a60ad4c88e1206b0063114f92a917c00ad6f
Tanja-M <tanja.mjagceev@gmx.de>
2020-11-10 12:09:08 +0100

    kitty spawns
    but on the wrong position

--- DemoProject/Assets/World4/Scripts/KittySpawner.cs
+++ DemoProject/Assets/World4/Scripts/KittySpawner.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using TMPro.Examples;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -8,25 +10,22 @@ public class KittySpawner : MonoBehaviour
 {
     public Transform kittyPrefab;
     public Transform parent;
+    private int counter = 0;
 
     public void Update()
     {
-        //TODO: how do i get the pointereventdata?
-        //OnPointerDown();
-        Debug.Log("should a kitty be spawned?");
-    }
-
-    public void OnPointerDown(PointerEventData pointerEventData)
-    {
-        Debug.Log("clicked");
-        SpawnKitty(pointerEventData.position);
+        if (Input.GetMouseButtonDown(0))
+        {
+            SpawnKitty(Input.mousePosition);
+        }
     }
 
     private void SpawnKitty(Vector3 position)
     {
-        Debug.Log("kitty spawned");
+        Debug.Log(position);
         GameObject kitty = GameObject.Instantiate(kittyPrefab.gameObject, position, Quaternion.identity, parent);
+
+        kitty.name = "SuperKitty" + counter;
+        counter++;
     }
-
-
 }
