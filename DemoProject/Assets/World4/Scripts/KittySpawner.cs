918c8bbfb4e86c285d0e30734cfa12323d2182bd
Tanja-M <tanja.mjagceev@gmx.de>
2020-11-10 11:12:59 +0100

    worked on kitty spawner

--- DemoProject/Assets/World4/Scripts/KittySpawner.cs
+++ DemoProject/Assets/World4/Scripts/KittySpawner.cs
@@ -1,19 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class KittySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform kittyPrefab;
+    public Transform parent;
+
+    public void Update()
     {
-        
+        //TODO: how do i get the pointereventdata?
+        //OnPointerDown();
+        Debug.Log("should a kitty be spawned?");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerDown(PointerEventData pointerEventData)
     {
-        
+        Debug.Log("clicked");
+        SpawnKitty(pointerEventData.position);
+    }
+
+    private void SpawnKitty(Vector3 position)
+    {
+        Debug.Log("kitty spawned");
+        GameObject kitty = GameObject.Instantiate(kittyPrefab.gameObject, position, Quaternion.identity, parent);
     }
 
 
